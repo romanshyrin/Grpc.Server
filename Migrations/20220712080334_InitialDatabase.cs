597a4040b5e8241938442e76d882897e1b6d4203
romanshyrin <104902868+romanshyrin@users.noreply.github.com>
2022-07-14 23:52:42 +0300

    try /.....

---
+++ Migrations/20220712080334_InitialDatabase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GrpcServer.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "mykola.karpenko@brightstar-academy.com", "Mykola", "Karpenko" },
+                    { 2, "nina.vyzir@brightstar-academy.com", "Nina", "Vyzir" },
+                    { 3, "oleksandr.yevtushenko@brightstar-academy.com", "Oleksandr", "Yevtushenko" },
+                    { 4, "roman.shyrin@brightstar-academy.com", "Roman", "Shyrin" },
+                    { 5, "viktor.kozhyn@brightstar-academy.com", "Viktor", "Кozhyn" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
